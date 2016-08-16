47afaa0586a2776d44fdfcc964a333286dcf29d5
Peter Kaleta <ext.pkaleta@riotgames.com>
2016-08-16 16:37:38 +0200

    live edit

--- examples/pages/card.jsx
+++ examples/pages/card.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import * as X from '../../src/';
 import AppBar from 'react-toolbox/lib/app_bar';
+import Playground from 'component-playground';
 
+import * as X from '../../src/';
 const {XteamerCard} = X;
 import xteamer from './fixtures/xteamer';
+import {Card as CardSample} from './samples'
 
 export default class Examples extends React.Component {
 
   render() {
     return (
-      <XteamerCard xteamer={xteamer}/>
-    );
+      <Playground
+        theme="monokai"
+        collapsableCode={true}
+        initiallyExpanded={true}
+        codeText={CardSample}
+        scope={{React, XteamerCard}}/>
+      );
   }
 }
---
+++ examples/pages/samples/card.js
@@ -0,0 +1,3 @@
+import xteamer from '../fixtures/xteamer.json';
+
+export default `<XteamerCard xteamer={${JSON.stringify(xteamer, null, '\t')}}/>`;
