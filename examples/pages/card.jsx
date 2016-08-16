4a1bd52f77a3b495125774b77a4f1c1d735837ba
Peter Kaleta <ext.pkaleta@riotgames.com>
2016-08-16 23:06:11 +0200

    docs layout

--- examples/pages/card.jsx
+++ examples/pages/card.jsx
@@ -11,12 +11,18 @@ export default class Examples extends React.Component {
 
   render() {
     return (
-      <Playground
+      <div>
+        <h2>Xteamer card</h2>
+        <p>Some basic description</p>
+        <p>Some specs </p>
+        <h2>Playground</h2>
+        <Playground
         theme="monokai"
-        collapsableCode={true}
-        initiallyExpanded={true}
-        codeText={CardSample}
-        scope={{React, XteamerCard}}/>
-      );
+          initiallyExpanded={true}
+          codeText={CardSample}
+          scope={{React, XteamerCard}}/>
+      </div>
+
+    );
   }
 }
